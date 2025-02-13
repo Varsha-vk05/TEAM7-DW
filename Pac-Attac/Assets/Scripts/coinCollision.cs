d24a0189bf5df253c08603d35179bc8aeb322228
b-a-dehaan <dehaanbethany3@gmail.com>
2025-02-12 21:40:28 -0500

    more troubleshooting

--- Pac-Attac/Assets/Scripts/coinCollision.cs
+++ Pac-Attac/Assets/Scripts/coinCollision.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class coinCollision : MonoBehaviour
-{ 
+{
+    private Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         playerCollision playerColl = other.GetComponent<playerCollision>();
@@ -12,12 +19,20 @@ public class coinCollision : MonoBehaviour
         if (playerColl != null && !playerColl.isIt)
         {
             playerScore playerScore = other.GetComponent<playerScore>();
-
             if (playerScore != null)
             {
+                AnimationManager();
                 playerScore.AddScore(1);
                 Destroy(gameObject);
             }
+        }
+
+        public void AnimationManager();
+        {
+            Instantiante("collect1", Spawn.Position(0), Spawn.Rotation(0));
+            anim.Play("coinCollect");
+            Yeild WaitForSeconds(1f);
+            Destroy(gameObject);
         }
     }
 }
