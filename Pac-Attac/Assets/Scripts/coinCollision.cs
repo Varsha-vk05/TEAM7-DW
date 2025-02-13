0591243a38e3ee75576afdb5cf9538fc0fee5b23
b-a-dehaan <dehaanbethany3@gmail.com>
2025-02-12 20:51:44 -0500

    bug fix push

--- Pac-Attac/Assets/Scripts/coinCollision.cs
+++ Pac-Attac/Assets/Scripts/coinCollision.cs
@@ -3,16 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class coinCollision : MonoBehaviour
-{
-    public Animator anim;
-
-    void Start()
-    {
-        if(anim ==null)
-        {
-         anim = GetComponant<Animator>();
-        }
-    }
+{ 
     private void OnTriggerEnter2D(Collider2D other)
     {
         playerCollision playerColl = other.GetComponent<playerCollision>();
@@ -24,8 +15,6 @@ public class coinCollision : MonoBehaviour
 
             if (playerScore != null)
             {
-                anim.SetTrigger(collect); //triggers animation
-                yeild return new WaitForSeconds(1);
                 playerScore.AddScore(1);
                 Destroy(gameObject);
             }
