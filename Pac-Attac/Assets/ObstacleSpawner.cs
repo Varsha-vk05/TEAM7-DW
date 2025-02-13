74007c5cc7ab2c32fef65e3e01bdeb504eaa4d9f
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-02-12 22:50:52 -0500

    Object Collision
    Objects Spawn and are Collidable

--- Pac-Attac/Assets/ObstacleSpawner.cs
+++ Pac-Attac/Assets/ObstacleSpawner.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleSpawner : MonoBehaviour
+public class ObstacleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject obstaclePrefab; 
+    public float spawnInterval = 3f; 
+    private float spawnTimer;
+
+    public float spawnRangeX = 8f; 
+    public float spawnRangeY = 4f;
+
     void Start()
     {
-        
+        spawnTimer = spawnInterval;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        spawnTimer -= Time.deltaTime;
+
+        if (spawnTimer <= 0f)
+        {
+            SpawnObstacle();
+            spawnTimer = spawnInterval; 
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        float randomX = Random.Range(-spawnRangeX, spawnRangeX);
+        float randomY = Random.Range(-spawnRangeY, spawnRangeY);
+        Vector2 spawnPosition = new Vector2(randomX, randomY);
+
+        Instantiate(obstaclePrefab, spawnPosition, Quaternion.identity);
     }
 }
