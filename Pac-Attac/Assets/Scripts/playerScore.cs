2ae86c675692e2a3b8f972e754ba0477d6c3a225
b-a-dehaan <dehaanbethany3@gmail.com>
2025-02-13 14:43:41 -0500

    commit

--- Pac-Attac/Assets/Scripts/playerScore.cs
+++ Pac-Attac/Assets/Scripts/playerScore.cs
@@ -32,3 +32,60 @@ public class playerScore : MonoBehaviour
         }
     }
 }
+
+public class HighScoreManager : MonoBehaviour
+{
+    private Transform entryContainer;
+    private Transform entryTemplate;
+    private List<HighScoreEntry> highScoreEntryList;
+    private List<Transform> highScoreEntryTransformList;
+    private void Awake()
+    {
+        entryContainer = transform.Find("HighscoreentryContainer");
+        entryTemplate = transform.Find("HIghscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        highScoreEntryList = new List<HighScoreEntry>()
+        {
+
+        };
+        highScoreEntryTransformList = new List<Transform>();
+        foreach (HighScoreEntry highScoreEntry in highScoreEntryList)
+        {
+            CreateHighScoreEntryTransform(HighScoreEntry highScoreEntry, Transform container,List < Transform > transformList)
+        }
+    }
+    private void CreateHighScoreEntryTransform(HighScoreEntry highScoreEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight 30f;
+
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponant<entryRectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+        entryTransform.gameObject.SetActive(true);
+
+        int rank = i + 1;
+        string rankString;
+        switch (rank)
+        {
+            default:
+                rankString = rank + "TH"; break;
+            case 1: rankString = "1st"; break;
+            case 2: rankString = "2nd"; break;
+            case 3: rankString = "3rd"; break;
+        }
+        entryTransform.Find("postext").GetComponant<Text>().text = "";
+
+        int score = highScoreEntry.score;
+        entryTransform.Find("scoretext").GetComponant<Text>().text = "";
+
+        string name -highScoreEntry.name;
+            entryTransform.Find("nametext").GetComponant<Text>().text = "";
+
+        transformList.Add(entryTransform);
+
+
+
+    }
+}
