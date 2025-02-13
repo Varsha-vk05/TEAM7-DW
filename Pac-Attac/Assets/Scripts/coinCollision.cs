1c73c7a2056c56c81b3af4c9caf868c741a6ec44
b-a-dehaan <dehaanbethany3@gmail.com>
2025-02-12 20:13:25 -0500

    working on animation code

--- Pac-Attac/Assets/Scripts/coinCollision.cs
+++ Pac-Attac/Assets/Scripts/coinCollision.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class coinCollision : MonoBehaviour
 {
+    public Animator anim;
+
+    void Start()
+    {
+        if(anim ==null)
+        {
+         anim = GetComponant<Animator>();
+        }
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         playerCollision playerColl = other.GetComponent<playerCollision>();
@@ -15,6 +24,8 @@ public class coinCollision : MonoBehaviour
 
             if (playerScore != null)
             {
+                anim.SetTrigger(collect); //triggers animation
+                yeild return new WaitForSeconds(1);
                 playerScore.AddScore(1);
                 Destroy(gameObject);
             }
